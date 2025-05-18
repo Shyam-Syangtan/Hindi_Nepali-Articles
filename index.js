c84c2c0182928c99e339caac7d3e0260f77fa660
Shyam-Syangtan <shyamtaman97@gmail.com>
2025-05-18 18:42:39 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+document.getElementById('vocabBtn').addEventListener('click', function () {
+  alert('Vocabulary section coming soon!');
+});
+
+document.getElementById('grammarBtn').addEventListener('click', function () {
+  alert('Grammar section coming soon!');
+});
+
+document.getElementById('alphabetBtn').addEventListener('click', function () {
+  alert('Alphabet learning coming soon!');
+});
+
+document.getElementById('quizBtn').addEventListener('click', function () {
+  alert('Quiz will be available soon!');
+});
